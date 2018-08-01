02a9d66f4b9592f3473c8be93dbf9b688c3689a9
carlos-oliver <carlos.st.oliveira@gmail.com>
2018-08-01 00:47:28 -0300

    Indentificação dos elementos. Configuração do Capybara.

--- mark7/features/step_definitions/login_steps.rb
+++ mark7/features/step_definitions/login_steps.rb
@@ -1,9 +1,12 @@
   Dado("que eu acesso o sistema") do
-    pending # Write code here that turns the phrase above into concrete actions
+    visit 'https://mark7.herokuapp.com'
+    sleep 5
   end
   
   Quando("faço login com {string} e {string}") do |email, senha|
-    pending # Write code here that turns the phrase above into concrete actions
+    find('#login_email').set email
+    find('input[name=passoword]').set senha
+    sleep 10
   end
   
   Então("vejo a seguinte mensagem {string}") do |mensagem|
