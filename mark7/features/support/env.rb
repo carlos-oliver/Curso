2eed4815e699770b198321097baca92687f181b2
carlos-oliver <carlos.st.oliveira@gmail.com>
2018-08-01 23:29:43 -0300

    Refatoração de código Implementação de PO Implementação de steps Atualizar perfil

--- mark7/features/support/env.rb
+++ mark7/features/support/env.rb
@@ -3,6 +3,7 @@ require 'capybara/cucumber'
 
 Capybara.configure do |config|
     config.default_driver = :selenium_chrome
+    config.app_host = 'https://mark7.herokuapp.com'
 end
 
 Capybara.default_max_wait_time = 5
--- mark7/features/support/hooks.rb
+++ mark7/features/support/hooks.rb
@@ -1,4 +1,22 @@
-After('@logout') do
-  find('.navbar a[href*=dropdown]').click
-  find('.navbar a[href$=logout]').click
-end
+# REGEX ou Expressão Regular
+# Contem => *
+# termina com => $
+# começa com => ^
+
+Before do
+    @nav = Navbar.new
+    @side = Sidebar.new
+    @login_page = LoginPage.new
+    @tarefas_page = TarefasPage.new
+    @perfil_page = PerfilPage.new
+  end
+  
+  Before('@login') do
+    @login_page.acessa
+    @login_page.logar('eu@papito.io', '123456')
+  end
+  
+  After('@logout') do
+    @nav.sair
+  end
+  
