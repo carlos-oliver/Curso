454054731d2a80683c66a5efc02fd4b8926b46fd
carlos-oliver <carlos.st.oliveira@gmail.com>
2018-08-02 20:50:52 -0300

    Implementação e refatoração usando PO.

--- mark7/features/support/env.rb
+++ mark7/features/support/env.rb
@@ -2,8 +2,8 @@ require 'capybara'
 require 'capybara/cucumber'
 
 Capybara.configure do |config|
-    config.default_driver = :selenium_chrome
-    config.app_host = 'https://mark7.herokuapp.com'
+  config.default_driver = :selenium_chrome
+  config.app_host = 'https://mark7.herokuapp.com'
 end
 
 Capybara.default_max_wait_time = 5
--- mark7/features/support/hooks.rb
+++ mark7/features/support/hooks.rb
@@ -4,19 +4,18 @@
 # começa com => ^
 
 Before do
-    @nav = Navbar.new
-    @side = Sidebar.new
-    @login_page = LoginPage.new
-    @tarefas_page = TarefasPage.new
-    @perfil_page = PerfilPage.new
-  end
-  
-  Before('@login') do
-    @login_page.acessa
-    @login_page.logar('eu@papito.io', '123456')
-  end
-  
-  After('@logout') do
-    @nav.sair
-  end
-  
+  @nav = Navbar.new
+  @side = Sidebar.new
+  @login_page = LoginPage.new
+  @tarefas_page = TarefasPage.new
+  @perfil_page = PerfilPage.new
+end
+
+Before('@login') do
+  @login_page.acessa
+  @login_page.logar('eu@papito.io', '123456')
+end
+
+After('@logout') do
+  @nav.sair
+end
